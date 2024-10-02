e990061d01a157a5dc45922a7a610e16bee1d089
Arghya Sarkar <arghyasarkar.nolan@gmail.com>
2024-10-02 22:16:41 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import OpenNewProject from './components/Navbar'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <OpenNewProject></OpenNewProject>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../App.css"
+
+const OpenNewProject = () => {
+  return (
+    <div className=" ">
+      {/* Main Container */}
+      <div className="container flex mx-auto mt-48">
+        <div className="pic1">
+            
+        </div>
+        <div className="card">
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OpenNewProject;
